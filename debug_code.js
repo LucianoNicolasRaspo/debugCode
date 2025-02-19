dc310e5347e97e25c8d34b6485a5d664cd4a732e
Luciano Raspo <lucianoraspo@outlook.com>
2025-02-19 17:47:30 -0300

    Agregando Operaciones Aritmeticas

--- debug_code.js
+++ debug_code.js
@@ -1,21 +1,47 @@
 function performOperation(){
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
+    let result;
+    const operation = document.getElementById('operation').value;
 
     if(!isNaN(num1) && !isNaN(num2)){
-        let result = multiply(num1, num2);
+        switch(operation){
+            case "suma":
+                result = sum(num1, num2);
+            break;
+            case "resta":
+                result = res(num1, num2);
+            break;
+            case "multiplicacion":
+                result = mul(num1, num2);
+            break;
+            case "division":
+                result = div(num1, num2);
+            break;
+        }
 
         displayResult(result);
     } else {
         displayResult('Please enter valid numbers');
     }
 }
-function multiply(a, b) {
-    // Introduce a debugger statement to pause execution
+
+function sum(a, b){
     debugger;
-    // Multiply the numbers
+    return a + b;
+}
+function res(a, b){
+    debugger;
+    return a - b;
+}function mul(a, b){
+    debugger;
     return a * b;
 }
+function div(a, b) {
+    debugger;
+    return a / b;
+}
+
 function displayResult(result) {
     // Display the result in the paragraph element
     const resultElement = document.getElementById('result');
